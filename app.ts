3504b238ec95f18ea7fd7a08d84b2619bbc7bd68
Gabriel.meduza <gabriel@meduza.digital>
2023-11-29 19:29:07 -0300

    fix:curso

--- app.ts
+++ app.ts
@@ -2,12 +2,12 @@ import { CompanyAccount } from './CompanyAccount';
 
 import { PeopleAccount } from './PeopleAccount';
 
-// const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Gb', 10);
+// const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Gabriel', 10);
 // console.log(peopleAccount)
 
 
 //  peopleAccount.deposit();
 
- const companyAccount: CompanyAccount = new CompanyAccount('Dio', 10);
+ const companyAccount: CompanyAccount = new CompanyAccount('Gabriel Ortiz', 10);
   // companyAccount.deposit();
 console.log(companyAccount.accountNumber)
